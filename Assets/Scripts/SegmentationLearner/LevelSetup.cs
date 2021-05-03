f5b3f3c6eb6fab3c982866a3dd93b456e636f497
carrotpie@gmail.com <carrotpie@gmail.com>
2021-05-03 18:56:22 +0300

    Fix to include all materials under the renderer; base jupyter notebook with data import

--- Assets/Scripts/SegmentationLearner/LevelSetup.cs
+++ Assets/Scripts/SegmentationLearner/LevelSetup.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelSetup : MonoBehaviour {
+public class LevelSetup : MonoBehaviour
+{
     public GameObject environment;
     public Material labelMaterial;
 
@@ -11,42 +12,61 @@ public class LevelSetup : MonoBehaviour {
     public int totalUniqueLabels;
     List<string> labeledNames = new List<string>();
 
-    void Start() {
+    void Start()
+    {
         SetupEnv();
         totalUniqueLabels = labeledNames.Count;
         CsvWriter.SaveLabels(LabelsBucket.GetLabels());
     }
 
-    void SetupEnv() {
+    void SetupEnv()
+    {
         labelEnvironment = Instantiate(environment, environment.transform.position, Quaternion.identity, environment.transform.parent);
         labelEnvironment.name = "Label Environment";
         SetChildrenLayer(labelEnvironment.transform, "LabelLayer");
     }
 
-    void SetChildrenLayer(Transform tr, string layer) {
+    void SetChildrenLayer(Transform tr, string layer)
+    {
         Transform trs = tr.GetComponentInChildren<Transform>(true);
-        foreach (Transform t in trs) {
+        foreach (Transform t in trs)
+        {
             t.gameObject.layer = LayerMask.NameToLayer(layer);
             TrySetLabelMat(t);
             SetChildrenLayer(t, layer);
         }
     }
 
-    void TrySetLabelMat(Transform tr) {
-        MeshRenderer rend = tr.GetComponent<MeshRenderer>();
-        if (rend != null) {
-            rend.material = labelMaterial;
-            LabelIdentity identity = rend.GetComponent<LabelIdentity>();
-            if (identity != null) {
-                rend.material.color = LabelsBucket.GetLabel(identity).encoderColor;
-                totalLabeledItems++;
-                if (!labeledNames.Contains(identity.labelName))
-                    labeledNames.Add(identity.labelName);
-            } else {
-                Debug.Log("identity component not found on: " + tr.name + " each renderable must have an LabelIdentity component with a type set.");
+    void TrySetLabelMat(Transform tr)
+    {
+        Renderer rend = tr.GetComponent<Renderer>();
+        if (rend != null)
+        {
+            Material[] curMats = rend.materials;
+            for (int i = 0; i < rend.materials.Length; i++)
+            {
+                curMats[i] = new Material(labelMaterial);
+                //rend.materials[i] = labelMaterial;
+                LabelIdentity identity = rend.GetComponent<LabelIdentity>();
+                if (identity != null)
+                {
+                    curMats[i].color = LabelsBucket.GetLabel(identity).encoderColor;
+                    totalLabeledItems++;
+                    if (!labeledNames.Contains(identity.labelName))
+                        labeledNames.Add(identity.labelName);
+                }
+                else
+                {
+                    Debug.Log("identity component not found on: " + tr.name + " each renderable must have an LabelIdentity component with a type set.");
+                }
             }
-        } else {
+            rend.materials = curMats;
+        }
+        else
+        {
             //Debug.Log("MechRenderer not found on: " + tr.name + " , won't set.");
         }
     }
+
+
 }
